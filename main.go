1a5a97ababfedcc9c6f1e5094c5a19b3d4d7c8ba
michalronin <michalronin@outlook.com>
2025-01-12 15:31:28 +0100

    Health status endpoint

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	filePathRoot := "./"
+	filePathRoot := "./app"
 	port := "8080"
 	mux := http.NewServeMux()
 	server := &http.Server{
@@ -14,7 +14,14 @@ func main() {
 		Addr:    ":" + port,
 	}
 	handler := http.FileServer(http.Dir("./"))
-	mux.Handle("/", handler)
+	mux.Handle("/app/", http.StripPrefix("/app/", handler))
+	mux.HandleFunc("/healthz", readinessHandler)
 	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
